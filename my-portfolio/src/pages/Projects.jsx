22e4e3cd5592b324d6ca5ec11e605e46e9512449
Dawson Bursey <150042132+Dawson-Bursey@users.noreply.github.com>
2025-04-02 12:28:51 -0230

    Resume page, added certifications

--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
         <div>
          
         
-            <Footer/>
+            
 
         </div>
 );
--- my-portfolio/src/pages/Resume.jsx
+++ my-portfolio/src/pages/Resume.jsx
@@ -1,11 +1,45 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import "./ResumePage.css";
+import "../index.css";
 
 function Resume() {
     return(
-        <div>
-            <NavBar/>
+        <div className="resumePage">
+            
+
+            <div className="contactInfo">
+            <h2>Certifications</h2>
+                <ul>
+                    <li>Phone: +1 (709)-573-5561</li>
+                    <li>Email: dawsonbursey04@outlook.com</li>
+                    <li>LinkedIn: linkedin.com/in/dawson-bursey</li>
+                    <li>GitHub: github.com/dawsonbursey</li>
+                </ul>
+            </div>
+
+            <div className="certifications">
+                <h2>Certifications</h2>
+                <ul>
+                    <li>Full-Stack Web Development Certificate - University of Toronto</li>
+                    <li>Google Data Analytics Certificate - Google</li>
+                    <li>Google IT Support Certificate - Google</li>
+                    <li>Google Project Management Certificate - Google</li>
+                </ul>
+            </div>
+
+            {/* <div className="education"></div>
+
+            <div className="experience"></div>
+
+            <div className="skills"></div>
+
+             */}
+
         
-            <Footer/>
+           
+        
+    
 
         </div>
 );
