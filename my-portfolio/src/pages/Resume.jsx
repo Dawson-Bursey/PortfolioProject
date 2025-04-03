091220651b159a569b45e78102af954f0329c762
Dawson Bursey <150042132+Dawson-Bursey@users.noreply.github.com>
2025-04-03 15:29:28 -0230

    Added Education section

--- my-portfolio/src/pages/Resume.jsx
+++ my-portfolio/src/pages/Resume.jsx
@@ -9,11 +9,14 @@ function Resume() {
             
 
             <div className="contactInfo">
-            <h2>Certifications</h2>
+            <h2>Contact Information</h2>
                 <ul>
                     <li>Phone: +1 (709)-573-5561</li>
+                    <br />
                     <li>Email: dawsonbursey04@outlook.com</li>
+                    <br />
                     <li>LinkedIn: linkedin.com/in/dawson-bursey</li>
+                    <br />
                     <li>GitHub: github.com/dawsonbursey</li>
                 </ul>
             </div>
@@ -21,14 +24,23 @@ function Resume() {
             <div className="certifications">
                 <h2>Certifications</h2>
                 <ul>
-                    <li>Full-Stack Web Development Certificate - University of Toronto</li>
-                    <li>Google Data Analytics Certificate - Google</li>
-                    <li>Google IT Support Certificate - Google</li>
-                    <li>Google Project Management Certificate - Google</li>
+                    <li>Front-End Software Developer</li>
+                    <li>Back-End Software Developer</li>
+                    <li>Software Foundations</li>
+                    <li>Dev-Ops and Software Design</li>
                 </ul>
             </div>
 
-            {/* <div className="education"></div>
+            <div className="education">
+                <h2>Education</h2>
+                <ul>
+                    <li>Keyin Collage</li>
+                    <li></li>
+                    <li>Graduated: 2023</li>
+                </ul>
+            </div>
+
+            {/* 
 
             <div className="experience"></div>
 
